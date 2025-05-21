a410b49ca5314ae977e8e007ff9992ab1d3596b7
AndrejYaotsin <andrej.butalov@gmx.de>
2025-05-21 13:40:01 +0200

    add assigned_user

--- join_app/models.py
+++ join_app/models.py
@@ -44,7 +44,7 @@ class Task(models.Model):
     status = models.CharField(max_length=50)
     date = models.DateField()
     subtasks = models.ManyToManyField(SubTask, blank=True)
-    # assigned_users = models.ManyToManyField(User, blank=True, related_name='tasks')
+    assigned_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks')
     assigned_guests = models.ManyToManyField(GuestContact, blank=True, related_name='tasks')
     
 
